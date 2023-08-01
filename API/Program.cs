f9dc6da7455b1f5fae599c170d1da4208581ba3a
Lucas Alves <lucasalves94@gmail.com>
2023-07-31 21:29:18 -0300

    basic sql configs on sqlite before migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,24 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container / dependency injection container
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+}
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. / order is important here
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
