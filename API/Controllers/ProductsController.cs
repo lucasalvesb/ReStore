8068323accaa5149895eefd02e24bce1ab04645f
Lucas Alves <lucasalves94@gmail.com>
2023-09-24 18:11:29 -0300

    finished get for products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -7,18 +9,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-    private readonly StoreContext context;
+    private readonly StoreContext _context;
+
         public ProductsController(StoreContext context)
         {
-            this.context = context;
-            
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Products>> GetProducts()
+        public async Task <ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-            return Ok(products);
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task <ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }
