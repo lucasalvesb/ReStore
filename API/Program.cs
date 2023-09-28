509d501d0b690b091daa896683aabc3600ed91d2
Lucas Alves <lucasalves94@gmail.com>
2023-09-27 22:11:23 -0300

    corrected to http on origin

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+
+
 app.UseAuthorization();
 
 app.MapControllers();
