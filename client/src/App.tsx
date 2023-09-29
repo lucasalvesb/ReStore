be635035fa71dc55745a29884b79fcaff750a353
Lucas Alves <lucasalves94@gmail.com>
2023-09-28 21:43:27 -0300

    added interface to products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
+import { Product } from "./Product";
 
 function App() {
-  const [products, setProducts] = useState([
-  {name: 'product1', price: 1},
-  {name: 'product2', price: 2},
-]);
+  const [products, setProducts] = useState<Product[]>([]);
 
 useEffect(() => {
   fetch('http://localhost:5000/api/products')
@@ -13,15 +11,22 @@ useEffect(() => {
 }, [])
 
   function addProduct() {
-    setProducts(prevState =>  [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+    setProducts(prevState =>  [...prevState,
+       {id: prevState.length + 101,
+        name: 'product' + (prevState.length + 1), 
+       price: (prevState.length * 100) + 100,
+       brand: 'some brand',
+       description: 'some description',
+       pictureUrl: 'https://picsum.photos/200/300'
+      }])
   }
 
   return (
     <div>
       <h1>Re-Store</h1> 
       <ul>
-        {products.map(item => (
-          <li key={item.name}>{item.name} - {item.price}</li>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       <button onClick={addProduct}>Add Product</button>
