09813e871cb765e64b08eec7618ebe9607ced3ae
Lucas Alves <lucasalves94@gmail.com>
2023-10-01 12:11:30 -0300

    added material ui and styled the list and buttom to add new component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/Product";
+import Catalog from "../../features/catalog/Catalog";
+import { Typography } from "@mui/material";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -22,15 +24,10 @@ useEffect(() => {
   }
 
   return (
-    <div>
-      <h1>Re-Store</h1> 
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
-    </div>
+    <>
+      <Typography variant="h1">Re-Store</Typography> 
+      <Catalog products={products} addProduct={addProduct}/>
+    </>
   );
 }
 
