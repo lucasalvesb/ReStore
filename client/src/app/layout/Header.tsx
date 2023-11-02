9c0e91ae74b3da679922c9ffc819257a6d6a9763
Lucas Alves <lucasalves94@gmail.com>
2023-11-02 20:56:54 -0300

    navlink added for the routing pages

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,4 +1,16 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+  {title: 'catalog', path: '/catalog'},
+  {title: 'about', path: '/about'},
+  {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+  {title: 'login', path: '/login'},
+  {title: 'register', path: '/register'},
+]
 
 interface HeaderProps {
   darkMode: boolean;
@@ -14,6 +26,30 @@ export default function Header({ darkMode, toggleDarkMode }: HeaderProps) {
           checked={darkMode}
           onChange={toggleDarkMode}
         />
+        <List sx={{ display: 'flex'}}>
+          {midLinks.map(({title, path}) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{color: 'inherit', typography: 'h6'}}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <List sx={{ display: 'flex'}}>
+          {rightLinks.map(({title, path}) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{color: 'inherit', typography: 'h6'}}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   );
