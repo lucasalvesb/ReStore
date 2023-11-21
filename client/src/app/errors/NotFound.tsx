b2d8d58730d2487f5920e9f339cf0cb0c3f6cc93
Lucas Alves <lucasalves94@gmail.com>
2023-11-20 21:41:56 -0300

    adjusted not found for the products too

--- client/src/app/errors/NotFound.tsx
+++ client/src/app/errors/NotFound.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <Container component={Paper} sx={{height: 400}}>
-      <Typography gutterBottom variant="h3"> We could not find what you are looking for</Typography>
+      <Typography gutterBottom variant="h3"> We could not find what you are looking for!</Typography>
       <Divider />
       <Button fullWidth component={Link} to='/catalog'>Go back to shop</Button>
     </Container>
