6b26f5a45d89908deb4e90f7cad7f72d2da7c5bb
Lucas Alves <lucasalves94@gmail.com>
2024-03-30 22:31:07 -0300

    fixed some problems with the basket entity

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+    private readonly StoreContext _context;
+        public BasketController(StoreContext context)
+        {
+      _context = context;
+        }
+
+        //endpoint to fetch basket
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+            if(basket == null) return NotFound();
+
+            return basket;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
+        {
+            //get basket
+            //create a basket if it doesn't exist
+            //get product
+            //add item
+            //save changes
+            return StatusCode(201);
+        }
+
+        //[HttpDelete]
+        //public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+    }
+}
