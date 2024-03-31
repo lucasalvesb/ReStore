823474406f5b900d585f25999fd957e2463caea6
Lucas Alves <lucasalves94@gmail.com>
2024-03-30 23:06:44 -0300

    more bug fixes

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -13,42 +13,22 @@ namespace API.Entities
 
         public void AddItem(Product product, int quantity)
         {
-            var item = Items.FirstOrDefault(i => i.ProductId == product.Id);
-            if (item == null)
+            if (Items.All(item => item.ProductId != product.Id))
             {
-                Items.Add(new BasketItem
-                {
-                    Product = product,
-                    Quantity = quantity
-                });
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
             }
-            else
-            {
-                item.Quantity += quantity;
-            }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (existingItem != null) existingItem.Quantity += quantity;
         }
 
         public void RemoveItem(int productId, int quantity) 
         {
             var item = Items.FirstOrDefault(i => i.ProductId == productId);
-            if (item != null)
-            {
-                if (item.Quantity > quantity)
-                {
-                    item.Quantity -= quantity;
-                }
-                else
-                {
-                    Items.Remove(item);
-                }
-            }
-
-            /* option 2 if the one at the top doesn't work properly with 0
-            var item = Items.FirstOrDefault(i => i.ProductId == productId);
             if (item == null) return;
             item.Quantity -= quantity;
             if (item.Quantity == 0) Items.Remove(item);
-            */
+
         }
     }
 }
--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0895a7ae048cf2db7fc7164bf99d48e28e910b90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
