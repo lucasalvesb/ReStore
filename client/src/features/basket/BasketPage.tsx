32de364a830967a29a0d0fc7586586507daf8bd5
Lucas Alves <lucasalves94@gmail.com>
2024-08-08 22:33:50 -0300

    added images to basket page

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 import { useStoreContext } from "../../app/context/StoreContext";
 import agent from "../../app/api/agent";
@@ -35,7 +35,10 @@ export default function BasketPage() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {item.name}
+                <Box display='flex' alignItems='center'>
+                  <img src={item.pictureUrl} alt={item.name} style={{height: 50, marginRight: 20}} />
+                  <span>{item.name}</span>
+                </Box>
               </TableCell>
               <TableCell align="right">${(item.price/100).toFixed(2)}</TableCell>
               <TableCell align="right">{item.quantity}</TableCell>
