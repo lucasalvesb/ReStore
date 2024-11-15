c61545c044f5eb0cb6b97dfd38cfc8b6b9e59a07
Lucas Alves <lucasalves94@gmail.com>
2024-11-15 17:25:36 -0300

    toolkit basics

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
-import counterReducer from "../../features/contact/counterReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../../features/contact/counterSlice";
 
-export function configureStore() {
-  return createStore(counterReducer)
-}
+// export function configureStore() {
+//   return createStore(counterReducer)
+// }
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
