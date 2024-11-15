34e7fd22c31483fca71f5ece8d0a47037c826100
Lucas Alves <lucasalves94@gmail.com>
2024-11-15 17:35:09 -0300

    finished basic test on toolkit redux

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "../../features/contact/counterSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 // export function configureStore() {
 //   return createStore(counterReducer)
@@ -13,3 +14,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
