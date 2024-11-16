7644efe777a9c0446f837f83c6f57432e39b250e
Lucas Alves <lucasalves94@gmail.com>
2024-11-15 22:36:59 -0300

    cleanup

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -40,14 +40,6 @@ export const basketSlice = createSlice({
   reducers: {
     setBasket: (state, action) => {
       state.basket = action.payload
-    },
-    removeItem: (state, action) => {
-      const {productId, quantity} = action.payload
-      const itemIndex = state.basket?.items.findIndex(i => i.productId === productId)
-      if (itemIndex === -1 || itemIndex === undefined) return
-      state.basket!.items[itemIndex].quantity -= quantity;
-      if (state.basket?.items[itemIndex].quantity === 0) 
-        state.basket.items.splice(itemIndex, 1)
     }
   },
   extraReducers: (builder => {
@@ -71,6 +63,7 @@ export const basketSlice = createSlice({
       state.basket!.items[itemIndex].quantity -= quantity!;
       if (state.basket?.items[itemIndex].quantity === 0) 
         state.basket.items.splice(itemIndex, 1)
+      state.status = 'idle'
     })
     builder.addCase(removeBasketItemAsync.rejected, (state) => {
       state.status = 'idle'
@@ -78,4 +71,4 @@ export const basketSlice = createSlice({
   })
 })
 
-export const {setBasket, removeItem} = basketSlice.actions
+export const {setBasket} = basketSlice.actions
