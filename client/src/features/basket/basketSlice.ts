16ac1ae110eb756129753a1a064e96e677e53b7d
Lucas Alves <lucasalves94@gmail.com>
2024-11-15 22:36:14 -0300

    changes to new add cases for async

--- client/src/features/basket/basketSlice.ts
+++ client/src/features/basket/basketSlice.ts
@@ -23,6 +23,17 @@ export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, q
   }
 )
 
+export const removeBasketItemAsync = createAsyncThunk<void, {productId: number, quantity?: number}>(
+  'basket/removeBasketItemAsync',
+  async  ({productId, quantity = 1}) => {
+    try {
+      await agent.Basket.removeItem(productId, quantity)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+)
+
 export const basketSlice = createSlice({
   name: 'basket',
   initialState,
@@ -48,6 +59,20 @@ export const basketSlice = createSlice({
       state.status = 'idle'
     })
     builder.addCase(addBasketItemAsync.rejected, (state) => {
+      state.status = 'idle'
+    })
+    builder.addCase(removeBasketItemAsync.pending, (state, action) => {
+      state.status = 'pendingRemoveItem' + action.meta.arg.productId
+    })
+    builder.addCase(removeBasketItemAsync.fulfilled, (state, action) => {
+      const {productId, quantity} = action.meta.arg
+      const itemIndex = state.basket?.items.findIndex(i => i.productId === productId)
+      if (itemIndex === -1 || itemIndex === undefined) return
+      state.basket!.items[itemIndex].quantity -= quantity!;
+      if (state.basket?.items[itemIndex].quantity === 0) 
+        state.basket.items.splice(itemIndex, 1)
+    })
+    builder.addCase(removeBasketItemAsync.rejected, (state) => {
       state.status = 'idle'
     })
   })
